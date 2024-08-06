53a5ac079db5a16ac9cd7eb66eeb3b2f1ceeb158
LeonardoGarciaMelo <lgarciamelo213@gmail.com>
2024-08-06 16:51:36 -0300

    Adding the piece images and creation of MoveType class in ChessLogic

--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0573f1d918b702687e2a5ffcccc18358f9bd9e4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24724e53ee4bce2a0775f6c63c1916a2351642d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24724e53ee4bce2a0775f6c63c1916a2351642d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
