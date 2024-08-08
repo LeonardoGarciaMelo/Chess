cb6888ec92ac6eed182ce929787d1a96d949fc2a
LeonardoGarciaMelo <lgarciamelo213@gmail.com>
2024-08-08 09:03:40 -0300

    Creating moves

--- ChessLogic/MoveType.cs
+++ ChessLogic/MoveType.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
-    public class MoveType
+    public enum MoveType
     {
+        Normal,
+        CastleKS,
+        CastleQS,
+        DoublePawn,
+        EnPassant,
+        Pawn
     }
 }
--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24724e53ee4bce2a0775f6c63c1916a2351642d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a5ac079db5a16ac9cd7eb66eeb3b2f1ceeb158")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
