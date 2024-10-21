274859cb287c03ef5f5ba607960d6aacaf9ef5d2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-21 20:59:31 -0300

    Pause and Restart

--- ChessUI/obj/Release/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Release/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b62fa3c7e21574cc8b4344827cbceb613f4aaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba1ef83b3a0463587974cd0d27f951188338ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
