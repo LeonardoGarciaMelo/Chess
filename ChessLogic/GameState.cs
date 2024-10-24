5fd26dd0b09c9533a167d0f075cb2588f312f2f3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-23 21:28:42 -0300

    Threefold Repetition end reason

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -13,11 +13,17 @@ namespace ChessLogic
         public Result Result { get; private set; } = null ;
 
         private int noCaptureOrPawnMoves = 0;
+        private string stateString;
+
+        private readonly Dictionary<string, int> stateHistory = new Dictionary<string, int>();
 
         public GameState(Player player, Board board)
         {
             CurrentPlayer = player;
             Board = board;
+
+            stateString = new StateString(CurrentPlayer, board).ToString();
+            stateHistory[stateString] = 1;
         }
 
         public IEnumerable<Move> LegalMovesForPiece(Position pos)
@@ -40,12 +46,14 @@ namespace ChessLogic
             if (captureOrPawn)
             {
                 noCaptureOrPawnMoves = 0;
+                stateHistory.Clear();
             }
             else 
             {
                 noCaptureOrPawnMoves ++;
             }
             CurrentPlayer = CurrentPlayer.Opponent();
+            UpdateStateString();
             CheckForGameOver();
         }
         
@@ -82,6 +90,10 @@ namespace ChessLogic
             {
                 Result = Result.Draw(EndReason.FiftyMoveRule);
             }
+            else if (ThreefoldRepetition())
+            {
+                Result = Result.Draw(EndReason.ThreefoldRepetition);
+            }
 
         }
 
@@ -94,6 +106,26 @@ namespace ChessLogic
         {
             int fullMoves = noCaptureOrPawnMoves / 2;
             return fullMoves == 50;
+        }
+
+        private void UpdateStateString()
+        {
+            stateString = new StateString(CurrentPlayer, Board).ToString();
+
+            if (!stateHistory.ContainsKey(stateString))
+            {
+                stateHistory[stateString] = 1;
+            }
+            else
+            {
+                stateHistory[stateString]++;
+            }
+
+        }
+
+        private bool ThreefoldRepetition()
+        {
+            return stateHistory[stateString] == 3;
         }
     }
 }
---
+++ ChessLogic/StateString.cs
@@ -0,0 +1,145 @@
+﻿using System.Text;
+
+namespace ChessLogic
+{
+    public class StateString
+    {
+        private readonly StringBuilder stringBuilder = new StringBuilder();
+
+        public StateString(Player currentPlayer, Board board) 
+        {   
+            AddPiecePlacement(board);
+            stringBuilder.Append(' ');
+            AddCurrentPlayer(currentPlayer);
+            stringBuilder.Append(' ');
+            AddCastlingRights(board);
+            stringBuilder.Append(' ');
+            AddEnPassant(board, currentPlayer);
+
+        }
+
+        public override string ToString()
+        {
+            return stringBuilder.ToString();
+        }
+
+        private static char PieceChar(Piece piece) 
+        {
+            char c = piece.Type switch
+            {
+                PieceType.Pawn => 'p',
+                PieceType.Knight => 'n',
+                PieceType.Rook => 'r',
+                PieceType.Bishop => 'b',
+                PieceType.Queen => 'q',
+                PieceType.King => 'k',
+                _ => ' '
+            };
+
+            if (piece.Color == Player.White)
+            {
+                return char.ToUpper(c);
+            }
+
+            return c;
+        }
+
+        private void AddRowData(Board board, int row)
+        {
+            int empty = 0;
+
+            for (int c = 0; c < 8; c++)
+            {
+                if (board[row, c] == null)
+                {
+                    empty++;
+                    continue;
+                }
+
+                if (empty > 0)
+                {
+                    stringBuilder.Append(empty);
+                    empty = 0;
+                }
+
+                stringBuilder.Append(PieceChar(board[row, c]));
+            }
+
+            if (empty > 0)
+            {
+                stringBuilder.Append(empty);
+            }
+        }
+
+        private void AddPiecePlacement(Board board)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                if (r != 0)
+                {
+                    stringBuilder.Append('/');
+                }
+
+                AddRowData(board, r);
+            }
+        }
+
+        private void AddCurrentPlayer(Player currentPlayer)
+        {
+            if (currentPlayer == Player.White)
+            {
+                stringBuilder.Append('w');
+            }
+            else
+            {
+                stringBuilder.Append('b');
+            }
+        }
+
+        private void AddCastlingRights(Board board)
+        {
+            bool castleWKS = board.CastleRightKS(Player.White);
+            bool castleWQS = board.CastleRightQS(Player.White);
+            bool castleBKS = board.CastleRightKS(Player.Black);
+            bool castleBQS = board.CastleRightQS(Player.Black);
+
+            if (!(castleWKS || castleWQS || castleBKS || castleBQS))
+            {
+                stringBuilder.Append('-');
+                return;
+            }
+
+            if (castleWKS)
+            {
+                stringBuilder.Append('K');
+            }
+            if (castleWQS)
+            {
+                stringBuilder.Append('Q');
+            }
+            if (castleBKS)
+            {
+                stringBuilder.Append('k');
+            }
+            if (castleBQS) 
+            {
+                stringBuilder.Append('q');
+            }
+        }
+
+        private void AddEnPassant(Board board, Player currentPlayer)
+        {
+            if (!board.CanCaptureEnPassant(currentPlayer))
+            {
+                stringBuilder.Append('-');
+                return;
+            }
+
+            Position pos = board.GetPawnSkipPosition(currentPlayer.Opponent());
+            char file = (char)('a' + pos.Column);
+            int rank = 8 - pos.Row;
+            stringBuilder.Append(file);
+            stringBuilder.Append(rank);
+        }
+    }
+}
